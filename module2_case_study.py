9219dddf43b0e9f5b40187cd3bbd7d304437082d
nicksivytech <nicksivytech@gmail.com>
2026-03-30 20:44:59 -0400

    Add loop for inputting student names and GPAs

--- module2_case_study.py
+++ module2_case_study.py
@@ -1,1 +1,15 @@
+while True:
+    last_name = input("Enter last name (ZZZ to quit): ")
 
+    if last_name == "ZZZ":
+        break
+
+    first_name = input("Enter first name: ")
+    gpa = float(input("Enter GPA: "))
+
+    if gpa >= 3.5:
+        print(first_name, last_name, "made the Dean's List")
+    elif gpa >= 3.25:
+        print(first_name, last_name, "made the Honor Roll")
+    else:
+        print(first_name, last_name, "did not qualify")
